d2199ba1eeae9eb82412c3071f5917bc9824be75
Gabriel Martins <gsmartins96@hotmail.com>
2022-01-10 12:18:39 -0300

    ✅ Desafio finalizado

--- src/components/Card/CardAnimation/index.tsx
+++ src/components/Card/CardAnimation/index.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
 import { useWindowDimensions, ViewProps } from 'react-native';
 import {
+  Extrapolate,
+  interpolate,
   useAnimatedStyle,
   useSharedValue,
-  withTiming
+  withTiming,
+  runOnJS
 } from 'react-native-reanimated';
 
 import { AnimationContainer } from './styles';
@@ -19,15 +22,20 @@ export function CardAnimation({ children, ...rest }: CardAnimationProps) {
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
-      // TODO - setup animated style
+      opacity: cardOpacity.value,
+      transform: [{ translateX: cardOffset.value }]
     }
   })
 
   useEffect(() => {
-    /**
-     * TODO - setup cardOpacity.value and cardOffset.value with
-     * withTiming()
-     */
+    cardOpacity.value = withTiming(
+      1, 
+      { duration: 1000 },
+    ) 
+    cardOffset.value = withTiming(
+      0, 
+      { duration: 1000 },
+    )
   }, []);
 
   return (
